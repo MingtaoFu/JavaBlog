bbcf7588eb6b07af5da98b102b9e20fa8d20630e
oneHuster <540847140@qq.com>
2015-11-05 22:11:18 +0800

    DA: correct createTables

--- src/DA/Base.java
+++ src/DA/Base.java
@@ -43,8 +43,8 @@ public class Base {
                         "Date timestamp not null default CURRENT_TIMESTAMP, Type varchar(16) not null, Intro text, " +
                         "LogoUrl varchar(64) default '/static/logos/default', primary key (Id))",
                 "create table Article(ID int not null auto_increment, Title varchar(64) not null, Content text, " +
-                        "Time timestamp not null default CURRENT_TIMESTAMP, ModifyTime timestamp not null, primary key (Id))",
-                "create table Comment(Id int not null auto_increment, Content text not null, Time timestamp not null default ON UPDATE CURRENT_TIMESTAMP, " +
+                        "Time timestamp not null default CURRENT_TIMESTAMP, ModifyTime timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP , primary key (Id))",
+                "create table Comment(Id int not null auto_increment, Content text not null, Time timestamp not null default CURRENT_TIMESTAMP, " +
                         "ArticleId int not null, User varchar(32) not null, primary key (Id), " +
                         "foreign key (User) references User(Id))",
                 "create table Response(Id int not null auto_increment, Content text not null, Time timestamp not null default CURRENT_TIMESTAMP, " +
